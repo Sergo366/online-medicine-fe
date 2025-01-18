fb25aecb86c9380302ca6b5e31d29ca50843481d
skozenkov <skozenkov@deloitte.com>
2025-01-18 10:43:23 +0100

    change version of next js and react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,28 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next', 'prettier'],
-  }),
-]
-
-export default eslintConfig;
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    extends: [
+      'next/core-web-vitals',
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+    ],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detects the React version
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'on', // Disables the rule requiring React in scope
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
