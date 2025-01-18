99ccce30dc011cc8a6a58d4ea9fd75e85ed2f324
skozenkov <skozenkov@deloitte.com>
2025-01-18 11:54:09 +0100

    header and styles

---
+++ src/shared/components/RootHeader/RootHeader.tsx
@@ -0,0 +1,7 @@
+export const RootHeader = () => {
+  return (
+    <div className={'bg-dark text-light flex h-10 w-full justify-center'}>
+      headerheaderheaderheaderheaderheaderheader
+    </div>
+  );
+};
---
+++ src/shared/components/RootHeader/const.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  main: '/',
+  makeAppointment: '/make-appointment',
+  contacts: '/contacts',
+  aboutUs: '/aboutUs',
+};
